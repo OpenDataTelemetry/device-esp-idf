962d8166b93a1702dd1e3f6fbac54af92ced4301
rogeriocassares <roger10cassares@gmail.com>
2025-04-07 17:50:28 -0300

    add Kconfi params for esp32s3 ans esp32c6

--- main/app_main.c
+++ main/app_main.c
@@ -9,6 +9,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*NMEA*/
+#include <stdio.h>
+#include <stdlib.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
+#include "nmea_parser.h"
+
+static const char *TAG = "gps_demo";
+
+#define TIME_ZONE (0)   //Beijing Time
+#define YEAR_BASE (2000) //date in GPS starts from 2000
+
 /*CAN*/
 #include "freertos/queue.h"
 #include "freertos/semphr.h"
@@ -161,6 +174,33 @@ static void rx_task(void *arg)
   free(data);
 }
 
+
+static void gps_event_handler(void *event_handler_arg, esp_event_base_t event_base, int32_t event_id, void *event_data)
+{
+    gps_t *gps = NULL;
+    switch (event_id) {
+    case GPS_UPDATE:
+        gps = (gps_t *)event_data;
+        /* print information parsed from GPS statements */
+        ESP_LOGI(TAG, "%d/%d/%d %d:%d:%d => \r\n"
+                 "\t\t\t\t\t\tlatitude   = %.05f°N\r\n"
+                 "\t\t\t\t\t\tlongitude = %.05f°E\r\n"
+                 "\t\t\t\t\t\taltitude   = %.02fm\r\n"
+                 "\t\t\t\t\t\tspeed      = %fm/s",
+                 gps->date.year + YEAR_BASE, gps->date.month, gps->date.day,
+                 gps->tim.hour + TIME_ZONE, gps->tim.minute, gps->tim.second,
+                 gps->latitude, gps->longitude, gps->altitude, gps->speed);
+        break;
+    case GPS_UNKNOWN:
+        /* print unknown statements */
+        ESP_LOGW(TAG, "Unknown statement:%s", (char *)event_data);
+        break;
+    default:
+        break;
+    }
+}
+
+
 void app_main(void)
 {
   init();
@@ -193,4 +233,20 @@ void app_main(void)
   vSemaphoreDelete(rx_sem);
 
   send_gps_task();
+
+
+
+      /* NMEA parser configuration */
+      nmea_parser_config_t config = NMEA_PARSER_CONFIG_DEFAULT();
+      /* init NMEA parser library */
+      nmea_parser_handle_t nmea_hdl = nmea_parser_init(&config);
+      /* register event handler for NMEA parser library */
+      nmea_parser_add_handler(nmea_hdl, gps_event_handler, NULL);
+  
+      vTaskDelay(10000 / portTICK_PERIOD_MS);
+  
+      /* unregister event handler */
+      nmea_parser_remove_handler(nmea_hdl, gps_event_handler);
+      /* deinit NMEA parser library */
+      nmea_parser_deinit(nmea_hdl);
 }
