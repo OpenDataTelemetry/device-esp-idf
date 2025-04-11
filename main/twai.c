85de0b17aa91db7ae233eb371d77b4a31780ecdc
rogeriocassares <cassares.rogerio@gmail.com>
2025-04-11 02:07:24 -0300

    add twai to esp_events

--- main/twai.c
+++ main/twai.c
@@ -1,19 +1,31 @@
-/*UART ASYNC*/
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "esp_system.h"
-#include "esp_log.h"
-#include "driver/uart.h"
-#include "string.h"
-#include "driver/gpio.h"
+// /*UART ASYNC*/
+// #include "freertos/FreeRTOS.h"
+// #include "freertos/task.h"
+// #include "esp_system.h"
+// #include "esp_log.h"
+// #include "driver/uart.h"
+// #include "string.h"
+// #include "driver/gpio.h"
+// #include <stdio.h>
+// #include <stdlib.h>
+// /*CAN*/
+// #include "freertos/queue.h"
+// #include "freertos/semphr.h"
+// #include "esp_err.h"
+// #include "driver/twai.h"
+// #include "twai.h"
+
 #include <stdio.h>
 #include <stdlib.h>
-/*CAN*/
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 #include "freertos/queue.h"
 #include "freertos/semphr.h"
 #include "esp_err.h"
+#include "esp_log.h"
 #include "driver/twai.h"
 #include "twai.h"
+
 
 /*TWAI*/
 #define NO_OF_ITERS 3
@@ -29,54 +41,35 @@
 #define ID_SLAVE_DATA 0x0B1
 #define ID_SLAVE_PING_RESP 0x0B2
 
+ESP_EVENT_DEFINE_BASE(ESP_TWAI_EVENT);
+
 static const twai_filter_config_t f_config = TWAI_FILTER_CONFIG_ACCEPT_ALL();
+// static const twai_timing_config_t t_config = TWAI_TIMING_CONFIG_25KBITS();
 static const twai_timing_config_t t_config = TWAI_TIMING_CONFIG_1MBITS();
+// static const twai_timing_config_t t_config = TWAI_TIMING_CONFIG_1MBITS();
 // Set TX queue length to 0 due to listen only mode
-static const twai_general_config_t g_config = TWAI_GENERAL_CONFIG();
+static const twai_general_config_t g_config = {.mode = TWAI_MODE_LISTEN_ONLY,
+                                               .tx_io = TX_GPIO_NUM,
+                                               .rx_io = RX_GPIO_NUM,
+                                               .clkout_io = TWAI_IO_UNUSED,
+                                               .bus_off_io = TWAI_IO_UNUSED,
+                                               .tx_queue_len = 0,
+                                               .rx_queue_len = 5,
+                                               .alerts_enabled = TWAI_ALERT_NONE,
+                                               .clkout_divider = 0};
+
+// static const twai_filter_config_t f_config = TWAI_FILTER_CONFIG_ACCEPT_ALL();
+// static const twai_timing_config_t t_config = TWAI_TIMING_CONFIG_1MBITS();
+// // Set TX queue length to 0 due to listen only mode
+// static const twai_general_config_t g_config = TWAI_GENERAL_CONFIG();
 
 static SemaphoreHandle_t rx_sem;
 
-/* --------- TWAI TASK BEGIN ---------*/
-static void twai_configure(void *arg)
-{
-  esp_gps_t *esp_gps = (esp_gps_t *)arg;
-  uart_event_t event;
-  int len = 0;
-
-
-static const char *GPS_TASK_TAG = "GPS_TASK";
-  sendData(GPS_TASK_TAG, "AT\r\n");
-  vTaskDelay(1000 / portTICK_PERIOD_MS);
-  sendData(GPS_TASK_TAG, "AT\r\n");
-  vTaskDelay(1000 / portTICK_PERIOD_MS);
-  sendData(GPS_TASK_TAG, "AT\r\n");
-  vTaskDelay(1000 / portTICK_PERIOD_MS);
-
-  sendData(GPS_TASK_TAG, "AT+CGNSSPWR=1\r\n\r\n");
-  vTaskDelay(1000 / portTICK_PERIOD_MS);
-  sendData(GPS_TASK_TAG, "AT+CGPSHOT\r\n");
-  vTaskDelay(1000 / portTICK_PERIOD_MS);
-  
-  sendData(GPS_TASK_TAG, "AT+CGPSINFO=1\r\n");
-  vTaskDelay(1000 / portTICK_PERIOD_MS);
-  sendData(GPS_TASK_TAG, "AT+CGNSSINFO=1\r\n");
-  vTaskDelay(1000 / portTICK_PERIOD_MS);
-  sendData(GPS_TASK_TAG, "AT+CGNSSPORTSWITCH=0,1\r\n");
-  vTaskDelay(1000 / portTICK_PERIOD_MS);
-  sendData(GPS_TASK_TAG, "AT+CGNSSNMEA=1,1,1,1,1,1,0,0,0,0\\r\n");
-  vTaskDelay(10000 / portTICK_PERIOD_MS);
-
-  sendData(GPS_TASK_TAG, "AT+CGNSSTST=0\r\n");
-  vTaskDelay(1000 / portTICK_PERIOD_MS);
-  sendData(GPS_TASK_TAG, "AT+CGNSSTST=1\r\n");
-  vTaskDelay(1000 / portTICK_PERIOD_MS);
-
-  vTaskDelete(NULL);
-}
-
+/* --------- TWAI TASK INIT ---------*/
 static void twai_receive_task(void *arg)
 {
   xSemaphoreTake(rx_sem, portMAX_DELAY);
+  printf("##################ENTERED TWAI TASK!");
 
   while (1)
   {
@@ -96,7 +89,6 @@ static void twai_receive_task(void *arg)
 }
 /* --------- TWAI TASK END ---------*/
 
-//========================================
 /**
  * @brief Init TWAI
  *
@@ -105,130 +97,46 @@ static void twai_receive_task(void *arg)
  */
 twai_handle_t twai_init(const twai_config_t *twai_config)
 {
-  esp_gps_t *esp_gps = calloc(1, sizeof(esp_gps_t));
-  if (!esp_gps)
-  {
-    ESP_LOGE(TWAI_TAG, "calloc memory for esp_fps failed");
-    goto err_gps;
-  }
-  esp_gps->buffer = calloc(1,TWAI_RUNTIME_BUFFER_SIZE);
-  if (!esp_gps->buffer)
-  {
-    ESP_LOGE(TWAI_TAG, "calloc memory for runtime buffer failed");
-    goto err_buffer;
-  }
-#if CONFIG_NMEA_STATEMENT_GSA
-  esp_gps->all_statements |= (1 << STATEMENT_GSA);
-#endif
-#if CONFIG_NMEA_STATEMENT_GSV
-  esp_gps->all_statements |= (1 << STATEMENT_GSV);
-#endif
-#if CONFIG_NMEA_STATEMENT_GGA
-  esp_gps->all_statements |= (1 << STATEMENT_GGA);
-#endif
-#if CONFIG_NMEA_STATEMENT_RMC
-  esp_gps->all_statements |= (1 << STATEMENT_RMC);
-#endif
-#if CONFIG_NMEA_STATEMENT_GLL
-  esp_gps->all_statements |= (1 << STATEMENT_GLL);
-#endif
-#if CONFIG_NMEA_STATEMENT_VTG
-  esp_gps->all_statements |= (1 << STATEMENT_VTG);
-#endif
-  /* Set attributes */
-  esp_gps->uart_port = twai_config->uart.uart_port;
-  esp_gps->all_statements &= 0xFE;
-  /* Install UART friver */
-  uart_config_t uart_config = {
-      .baud_rate = twai_config->uart.baud_rate,
-      .data_bits = twai_config->uart.data_bits,
-      .parity = twai_config->uart.parity,
-      .stop_bits = twai_config->uart.stop_bits,
-      .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
-      .source_clk = UART_SCLK_DEFAULT,
-  };
-  if (uart_driver_install(esp_gps->uart_port, CONFIG_NMEA_PARSER_RING_BUFFER_SIZE, 0,
-    twai_config->uart.event_queue_size, &esp_gps->event_queue, 0) != ESP_OK)
-  {
-    ESP_LOGE(TWAI_TAG, "install uart driver failed");
-    goto err_uart_install;
-  }
-  if (uart_param_config(esp_gps->uart_port, &uart_config) != ESP_OK)
-  {
-    ESP_LOGE(TWAI_TAG, "config uart parameter failed");
-    goto err_uart_config;
-  }
-  if (uart_set_pin(esp_gps->uart_port, twai_config->uart.tx_pin, twai_config->uart.rx_pin,
-                   UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE) != ESP_OK)
-  {
-    ESP_LOGE(TWAI_TAG, "config uart gpio failed");
-    goto err_uart_config;
-  }
-  /* Set pattern interrupt, used to detect the end of a line */
-  uart_enable_pattern_det_baud_intr(esp_gps->uart_port, '\n', 1, 9, 0, 0);
-  /* Set pattern queue size */
-  uart_pattern_queue_reset(esp_gps->uart_port, twai_config->uart.event_queue_size);
-  uart_flush(esp_gps->uart_port);
-  /* Create Event loop */
-  esp_event_loop_args_t loop_args = {
-      .queue_size = NMEA_EVENT_LOOP_QUEUE_SIZE,
-      .task_name = NULL};
-  if (esp_event_loop_create(&loop_args, &esp_gps->event_loop_hdl) != ESP_OK)
-  {
-    ESP_LOGE(TWAI_TAG, "create event loop faild");
-    goto err_eloop;
-  }
+  rx_sem = xSemaphoreCreateBinary();
+  xTaskCreatePinnedToCore(twai_receive_task, "TWAI_rx", 4096, NULL, RX_TASK_PRIO, NULL, tskNO_AFFINITY);
 
-  //RC-EDIT
-  xTaskCreate(twai_configure, "twai_configure", CONFIG_NMEA_PARSER_TASK_STACK_SIZE, esp_gps, CONFIG_NMEA_PARSER_TASK_PRIORITY, NULL);
+  // Install and start TWAI driver
+  ESP_ERROR_CHECK(twai_driver_install(&g_config, &t_config, &f_config));
+  // ESP_ERROR_CHECK(twai_driver_install(&twai_config->g_config, &twai_config->t_config, &twai_config->f_config));
+  ESP_LOGI(TAG_TWAI, "Driver installed");
+  ESP_ERROR_CHECK(twai_start());
+  ESP_LOGI(TAG_TWAI, "Driver started");
 
-  /* Create NMEA Parser task */
-  BaseType_t err = xTaskCreate(
-      twai_task_entry,
-      "twai",
-      CONFIG_NMEA_PARSER_TASK_STACK_SIZE,
-      esp_gps,
-      CONFIG_NMEA_PARSER_TASK_PRIORITY,
-      &esp_gps->tsk_hdl);
-  if (err != pdTRUE)
-  {
-    ESP_LOGE(TWAI_TAG, "create NMEA Parser task failed");
-    goto err_task_create;
-  }
-  ESP_LOGI(TWAI_TAG, "NMEA Parser init OK");
-  return esp_gps;
-  /*Error Handling*/
-err_task_create:
-  esp_event_loop_delete(esp_gps->event_loop_hdl);
-err_eloop:
-err_uart_install:
-  uart_driver_delete(esp_gps->uart_port);
-err_uart_config:
-err_buffer:
-  free(esp_gps->buffer);
-err_gps:
-  free(esp_gps);
+  xSemaphoreGive(rx_sem); // Start RX task
+  vTaskDelay(pdMS_TO_TICKS(100));
+  xSemaphoreTake(rx_sem, portMAX_DELAY); // Wait for RX task to complete
+
+  // Stop and uninstall TWAI driver
+  ESP_ERROR_CHECK(twai_stop());
+  ESP_LOGI(TAG_TWAI, "Driver stopped");
+  ESP_ERROR_CHECK(twai_driver_uninstall());
+  ESP_LOGI(TAG_TWAI, "Driver uninstalled");
+
+  // Cleanup
+  vSemaphoreDelete(rx_sem);
+
   return NULL;
 }
-//========================================
-//========================================
 
-//TWAI INIT
-rx_sem = xSemaphoreCreateBinary();
-xTaskCreatePinnedToCore(twai_receive_task, "TWAI_rx", 4096, NULL, RX_TASK_PRIO, NULL, tskNO_AFFINITY);
-
-// Install and start TWAI driver
-ESP_ERROR_CHECK(twai_driver_install(&g_config, &t_config, &f_config));
-ESP_LOGI(TAG_TWAI, "Driver installed");
-ESP_ERROR_CHECK(twai_start());
-ESP_LOGI(TAG_TWAI, "Driver started");
-
-xSemaphoreGive(rx_sem); // Start RX task
-vTaskDelay(pdMS_TO_TICKS(100));
-xSemaphoreTake(rx_sem, portMAX_DELAY); // Wait for RX task to complete
-
-// Stop and uninstall TWAI driver
-ESP_ERROR_CHECK(twai_stop());
-ESP_LOGI(TAG_TWAI, "Driver stopped");
-ESP_ERROR_CHECK(twai_driver_uninstall());
-ESP_LOGI(TAG_TWAI, "Driver uninstalled");
+/**
+ * @brief Add user defined handler for NMEA parser
+ *
+ * @param twai handle of NMEA parser
+ * @param event_handler user defined event handler
+ * @param handler_args handler specific arguments
+ * @return esp_err_t
+ *  - ESP_OK: Success
+ *  - ESP_ERR_NO_MEM: Cannot allocate memory for the handler
+ *  - ESP_ERR_INVALIG_ARG: Invalid combination of event base and event id
+ *  - Others: Fail
+ */
+esp_err_t twai_register_event(twai_handle_t twai, twai_event_id_t event, esp_event_handler_t event_handler, void *event_handler_arg)
+{
+  return esp_event_handler_register_with(twai, ESP_TWAI_EVENT, event,
+                                         event_handler, event_handler_arg);
+}
--- main/twai.h
+++ main/twai.h
@@ -1,13 +1,50 @@
 #pragma once
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 
 #include "esp_types.h"
 #include "esp_event.h"
 #include "esp_err.h"
 #include "driver/twai.h"
+
+#define TWAI_MAX_SATELLITES_IN_USE (12)
+#define TWAI_MAX_SATELLITES_IN_VIEW (16)
+
+  /**
+   * @brief *MQTT* event types.
+   *
+   * User event handler receives context data in `esp_mqtt_event_t` structure with
+   *  - client - *MQTT* client handle
+   *  - various other data depending on event type
+   *
+   */
+  typedef enum twai_event_id_t
+  {
+    TWAI_EVENT_ANY = -1,
+    TWAI_UPDATE, /*!< TWAI information has been updated */
+    TWAI_UNKNOWN /*!< Unknown statements detected */
+
+  } twai_event_id_t;
+
+  /**
+   * @brief Declare of TWAI Event base
+   *
+   */
+  ESP_EVENT_DECLARE_BASE(ESP_TWAI_EVENT);
+
+  /**
+   * @brief TWAI object
+   *
+   */
+  typedef struct
+  {
+    float latitude;                           /*!< Latitude (degrees) */
+    float longitude;                          /*!< Longitude (degrees) */
+    float altitude; /*!< Altitude (meters) */ /*!< Magnetic variation */
+  } twai_t;
 
 /**
  * @brief Default configuration for TWAI
@@ -23,159 +60,35 @@ extern "C" {
       .tx_queue_len = 0,                 \
       .rx_queue_len = 5,                 \
       .alerts_enabled = TWAI_ALERT_NONE, \
-      .clkout_divider = 0};
+      .clkout_divider = 0}
 
-
-#define TWAI_MAX_SATELLITES_IN_USE (12)
-#define TWAI_MAX_SATELLITES_IN_VIEW (16)
-
-  /**
-   * @brief *MQTT* event types.
-   *
-   * User event handler receives context data in `esp_mqtt_event_t` structure with
-   *  - client - *MQTT* client handle
-   *  - various other data depending on event type
-   *
-   */
-  typedef enum twai_event_id_t
-  {
-    twai_EVENT_ANY = -1,
-
-  } twai_event_id_t;
-
-  /**
-   * @brief Declare of TWAI Event base
-   *
-   */
-  ESP_EVENT_DECLARE_BASE(ESP_NMEA_EVENT);
-
-  /**
-   * @brief TWAI fix type
-   *
-   */
-  typedef enum
-  {
-    TWAI_FIX_INVALID, /*!< Not fixed */
-    TWAI_FIX_TWAI,    /*!< TWAI */
-    TWAI_FIX_DTWAI,   /*!< Differential TWAI */
-  } twai_fix_t;
-
-  /**
-   * @brief TWAI fix mode
-   *
-   */
-  typedef enum
-  {
-    TWAI_MODE_INVALID = 1, /*!< Not fixed */
-    TWAI_MODE_2D,          /*!< 2D TWAI */
-    TWAI_MODE_3D           /*!< 3D TWAI */
-  } twai_fix_mode_t;
-
-  /**
-   * @brief TWAI satellite information
-   *
-   */
   typedef struct
   {
-    uint8_t num;       /*!< Satellite number */
-    uint8_t elevation; /*!< Satellite elevation */
-    uint16_t azimuth;  /*!< Satellite azimuth */
-    uint8_t snr;       /*!< Satellite signal noise ratio */
-  } twai_satellite_t;
-
-  /**
-   * @brief TWAI time
-   *
-   */
-  typedef struct
-  {
-    uint8_t hour;      /*!< Hour */
-    uint8_t minute;    /*!< Minute */
-    uint8_t second;    /*!< Second */
-    uint16_t thousand; /*!< Thousand */
-  } twai_time_t;
-
-  /**
-   * @brief TWAI date
-   *
-   */
-  typedef struct
-  {
-    uint8_t day;   /*!< Day (start from 1) */
-    uint8_t month; /*!< Month (start from 1) */
-    uint16_t year; /*!< Year (start from 2000) */
-  } twai_date_t;
-
-  /**
-   * @brief NMEA Statement
-   *
-   */
-  typedef enum
-  {
-    STATEMENT_UNKNOWN = 0, /*!< Unknown statement */
-    STATEMENT_GGA,         /*!< GGA */
-    STATEMENT_GSA,         /*!< GSA */
-    STATEMENT_RMC,         /*!< RMC */
-    STATEMENT_GSV,         /*!< GSV */
-    STATEMENT_GLL,         /*!< GLL */
-    STATEMENT_VTG          /*!< VTG */
-  } twai_statement_t;
-
-  /**
-   * @brief TWAI object
-   *
-   */
-  typedef struct
-  {
-    float latitude;                           /*!< Latitude (degrees) */
-    float longitude;                          /*!< Longitude (degrees) */
-    float altitude; /*!< Altitude (meters) */ /*!< Magnetic variation */
-  } twai_t;
-
-  /**
-   * @brief Configuration of TWAI
-   *
-   */
-  typedef struct
-  {
-    struct
-    {
-      uart_port_t uart_port; /*!< UART port number */
-      uint32_t tx_pin;       /*!< UART Tx Pin number */
-      uint32_t rx_pin;       /*!< UART Rx Pin number */
-      uint32_t baud_rate;    /*!< UART baud rate */
-    } twai;                  /*!< UART specific configuration */
+    twai_filter_config_t f_config;
+    twai_timing_config_t t_config;
+    twai_general_config_t g_config;
   } twai_config_t;
 
-  /**
-   * @brief TWAI Handle
-   *
-   */
-  typedef void *twai_handle_t;
+#define TWAI_CONFIG_DEFAULT()          \
+  {                                    \
+      TWAI_FILTER_CONFIG_ACCEPT_ALL(), \
+      TWAI_TIMING_CONFIG_1MBITS(),     \
+      TWAI_GENERAL_CONFIG()}
 
-/**
- * @brief Default configuration for TWAI
- *
- */
-#define twai_CONFIG_DEFAULT()                  \
-  {                                            \
-    .uart = {                                  \
-      .uart_port = UART_NUM_1,                 \
-      .tx_pin = CONFIG_twai_UART_TXD,          \
-      .rx_pin = CONFIG_twai_UART_RXD,          \
-      .baud_rate = CONFIG_twai_UART_BAUD_RATE, \
-    }                                          \
-  }
+  // /**
+  //  * @brief TWAI Handle
+  //  *
+  //  */
+  // typedef void *twai_handle_t;
 
   /**
    * @brief TWAI Event ID
    *
    */
-  typedef enum
-  {
-    TWAI_UPDATE, /*!< TWAI information has been updated */
-    TWAI_UNKNOWN /*!< Unknown statements detected */
-  } twai_event_id_t;
+  // typedef enum {
+  //   TWAI_UPDATE, /*!< TWAI information has been updated */
+  //   TWAI_UNKNOWN /*!< Unknown statements detected */
+  // } twai_event_id_t;
 
   /**
    * @brief Init TWAI
